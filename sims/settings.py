3da0fb3ca939d4c572430be7e2e743f16e8fecfc
john-okojere <johnokojere08@gmail.com>
2024-09-27 03:14:09 +0100

    modified:   sims/settings.py

--- sims/settings.py
+++ sims/settings.py
@@ -123,11 +123,16 @@ import os
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'ims/static'),  # Tells Django where your static files are stored
-]
 STATIC_ROOT = 'ims/static/'
 
+if not DEBUG:    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+    # and renames the files with unique names for each version to support long-term caching
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+# Default p
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
